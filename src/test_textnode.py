9309fd9ee85fd1f2a288887b2258fe12f98c24e2
curator <curator4@gmail.com>
2025-06-24 21:47:59 +0000

    ch2: Nodes, L6 textnode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -31,6 +31,26 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("testnode", TextType.BOLD)
         node2 = TextNode("testnode", "bold")
         self.assertNotEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_link(self):
+        node = TextNode("click me", TextType.LINK, url="https://example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "click me")
+        self.assertEqual(html_node.props, {"href": "https://example.com"})
+
+    def test_image(self):
+        node = TextNode("a cat", TextType.IMAGE, url="cat.jpg")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src": "cat.jpg", "alt": "a cat"})
 
 if __name__ == "__main__":
     unittest.main()
